fcd79648310729525371272c3f609a37bb2405a9
姑胥 <guxu.ygh@alibaba-inc.com>
2021-02-01 01:33:00 +0800

    add readme

---
+++ readme.md
@@ -0,0 +1,19 @@
+## SqlBox
+This is my personal sql toolBox
+
+### SqlTable
+```
+it's convenient to use this tool to verify some thoughts, eg. XA+MDL deadlock 
+
+step 1: init transactions with class Trx
+[Trx('connection1', '127.0.0.1', 3306, 'username', 'password'), Trx('connection2', '127.0.0.1', 3306, 'username', 'password')]
+
+step 2: define sql table, it will be executed row by row
+[
+	['use db7',								'use db7'							],
+	['select version()',					'select version()'					],
+	['update t1 set c2 = 1 where c1 = 1', 	'update t2 set c2 = 1 where c1 = 1'	],
+	['update t2 set c2 = 1 where c1 = 1', 	'update t1 set c2 = 1 where c1 = 1'	]
+]
+```
+
--- sqlTable.py
+++ sqlTable.py
@@ -8,13 +8,14 @@ import time
 it's convenient to use this tool to verify some thoughts, eg. XA+MDL deadlock 
 
 step 1: init transactions with class Trx
-[		Trx(...),				 Trx(...), 					Trx(...)	]
+[Trx('connection1', '127.0.0.1', 3306, 'username', 'password'), Trx('connection2', '127.0.0.1', 3306, 'username', 'password')]
 
 step 2: define sql table, it will be executed row by row
 [
-	[	'select version()', 	None	,					None	],	
-	[	None, 					'select version()'	,		None	],
-	[	None, 					None	,					'select version()'	]
+	['use db7',								'use db7'							],
+	['select version()',					'select version()'					],
+	['update t1 set c2 = 1 where c1 = 1', 	'update t2 set c2 = 1 where c1 = 1'	],
+	['update t2 set c2 = 1 where c1 = 1', 	'update t1 set c2 = 1 where c1 = 1'	]
 ]
 
 """
